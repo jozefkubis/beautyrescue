cfe51b62ba78965f9a463e7275adb218ddff9866
JozefK <164421631+jozefkubis@users.noreply.github.com>
2026-02-12 08:20:31 +0100

    chore: initialize project with Next.js, TypeScript, and Tailwind CSS
    - Add package.json with dependencies and scripts for Next.js and TypeScript
    - Create postcss.config.mjs for Tailwind CSS integration
    - Add tsconfig.json for TypeScript configuration

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center font-semibold text-7xl text-red-600">
+      BEAUTYRESCUE
+    </div>
+  )
+}
