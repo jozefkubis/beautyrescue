05effe45e06b606ed29558935e044e5ee97d17b1
JozefK <164421631+jozefkubis@users.noreply.github.com>
2026-02-26 21:56:34 +0100

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,41 @@
+import {
+  Open_Sans,
+  Playfair_Display_SC,
+  Poppins,
+  Roboto,
+  Roboto_Condensed,
+} from "next/font/google"
 import "./globals.css"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+})
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+  weight: ["400", "500", "600", "700"],
+})
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  variable: "--font-roboto-condensed",
+  weight: ["400", "500", "600", "700"],
+})
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: ["400", "500", "600", "700"],
+})
+
+const playfairDisplaySC = Playfair_Display_SC({
+  subsets: ["latin"],
+  variable: "--font-playfair-display-sc",
+  weight: ["400", "700", "900"],
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +44,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${poppins.variable} ${openSans.variable} ${robotoCondensed.variable} ${roboto.variable} ${playfairDisplaySC.variable}`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image"
+
 export default function Page() {
   return (
-    <div className="flex w-full h-screen items-center justify-center font-semibold text-7xl text-red-600">
+    <div className="flex w-full h-screen items-center justify-center font-semibold text-7xl text-redMain">
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={90}
+        height={90}
+        className="mr-4"
+      />
       BEAUTYRESCUE
     </div>
   )
