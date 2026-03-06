e5c485f0bc78667d97d6869f68b25ed6eaa459a4
jozef kubis <kubis.jozef@outlook.com>
2026-03-06 18:51:37 +0100

    feat: enhance About page layout with grid structure and detailed content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,33 @@
 export default function Page() {
   return (
-    <div className="flex w-full h-screen text-4xl justify-center items-center">
-      O nás
+    <div className="grid grid-cols-2 w-full h-screen justify-center items-center px-6 lg:px-20 2xl:px-44 py-8 lg:py-10">
+      <div>
+        <h1 className="italic py-8 lg:py-10">
+          <span className="text-6xl 2xl:text-7xl">O</span>nás
+        </h1>
+        <p className="text-grey-700 line leading-8 text-sm xl:text-base 2xl:text-lg">
+          „Krása je vonkajší manifest vnútorného zdravia.“ (Katie Brindle,
+          expertka v č ínskej medicíne) Beauty Rescue je štúdio profesionálnej
+          kozmetiky na oživenie či ozdravenie Vašej krásy. Náš tím pozostávajúci
+          zo zdravotníkov- lekára, záchranárov a maséra svoje medicínske
+          znalosti a zdravotnícke zručnosti už dlhé roky využíva v oblasti
+          kozmetiky a ozdravovania. Z portfólia našich služieb si môžete vybrať
+          od profesionálnej kozmetiky, a síce najširšieho spektra
+          mezoterapeutickej chémie, jej aplikačných spôsobov a iných
+          profesionálnych kozmetických prístrojových ošetrení, pokračujúc
+          lekárskou kozmetikou- botulotoxínom, kyselinou hyalurónovou,
+          biokompatibilnými niťami, ďalej relaxom v podobe masáží, ku ktorým
+          máme ako bonus fínsku saunu, základ pre náš detoxprogram, alebo je
+          príčinou prečo sa v sebe necítite dobre nejaký zdravotný problém
+          prinášajúci dyskomfort? Nech sa páči, lekárska akupunktúra. Cieľom i
+          filozofiou „Záchranky krásy“ nie je pretvoriť Vás podľa najnovších
+          estetických trendov v niečiu kópiu, ale odkryť Vašu unikátnu krásu,
+          zakryť Vaše nedostatky, spomaliť progresiu starnutia, oživiť a
+          zachrániť originál vo Vás, posilniť a podporiť Vaše zdravie – Vašu
+          krásu.
+        </p>
+      </div>
+      <div></div>
     </div>
   )
 }
